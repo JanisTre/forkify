cc38085892c3d49a961e50e0346ae3a1722e1d9e
JanisTre <poxx@inbox.lv>
2019-04-07 10:05:56 +0100

    some error

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '@babel/polyfill';
+
+import * as str from './models/Search';
+import * as searchView from './views/searchView';
+//import {add ,multiply, ID}from './views/searchView';
+
+//import * from './views/searchView';
+//console.log('Using imoprted ${add(ID,2)} and ${multiply(3,5)}. ${str}');
+
